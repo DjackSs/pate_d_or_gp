46f8cf39544ebd506de28ec9fa9a36ef013a30d1
Pierre-Olivier M <po.mpolesha@gmail.com>
2024-01-22 16:29:33 +0100

    Restaurant details page

---
+++ src/main/java/dal/CardDAO.java
@@ -0,0 +1,135 @@
+package dal;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import bo.Card;
+
+public class CardDAO implements GenericDAOInterface<Card> {
+	private static final String TABLE_NAME = " cards ";
+
+	private static final String DELETE = "DELETE FROM"+ TABLE_NAME +" WHERE id = ?";
+	private static final String UPDATE = "UPDATE "+ TABLE_NAME +" SET name = ? WHERE id = ?";
+	private static final String INSERT = "INSERT INTO "+ TABLE_NAME +" (name) VALUES (?)";
+	private static final String SELECT_BY_ID = "SELECT * FROM "+ TABLE_NAME +" WHERE id = ?";
+	private static final String SELECT = "SELECT * FROM "+ TABLE_NAME;
+
+	private Connection cnx;
+
+	public CardDAO() throws DALException {
+		try
+		{
+			Context context = new InitialContext();
+			DataSource dataSource = (DataSource) context.lookup("java:comp/env/patedor");
+			
+			this.cnx = dataSource.getConnection();
+			
+			if(!cnx.isClosed())
+			{
+				System.out.println("la co est ouverte");
+			}
+		
+		
+		} 
+		catch (SQLException error) 
+		{
+			
+			throw new DALException("erreur de conexion à la base de donnée", error);
+		}
+		catch (NamingException e) 
+		{
+			e.printStackTrace();
+		}
+			
+	}
+
+	public List<Card> selectAll() throws DALException {
+		List<Card> cards = new ArrayList<>();
+
+		try {
+			PreparedStatement ps = cnx.prepareStatement(SELECT);
+			ResultSet rs = ps.executeQuery();
+
+			while(rs.next()) {
+				Card card = new Card();
+				card.setId(rs.getInt("id"));
+				card.setName(rs.getString("name"));
+
+				cards.add(card);
+			}
+		} catch (SQLException e) {
+			throw new DALException("Impossible de recuperer les informations", e);
+		}
+		return cards;
+	}
+
+	public Card selectById(int id) throws DALException {
+		Card card = null;
+
+		try {
+			PreparedStatement ps = cnx.prepareStatement(SELECT_BY_ID);
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+
+			if(rs.next()) {
+				card = new Card();
+				card.setId(rs.getInt("id"));
+				card.setName(rs.getString("name"));
+			}
+
+		} catch (SQLException e) {
+			throw new DALException("Impossible de recuperer les informations pour l'id "+ id, e);
+		}
+		return card;
+	}
+
+	public void insert(Card card) throws DALException {
+		try {
+			PreparedStatement ps = cnx.prepareStatement(INSERT, PreparedStatement.RETURN_GENERATED_KEYS);
+			ps.setString(1, card.getName());
+			ps.executeUpdate();
+
+			ResultSet rs = ps.getGeneratedKeys();
+			
+			if(rs.next()) {
+				int id = rs.getInt(1);
+				card.setId(id);
+			}
+		} catch (SQLException e) {
+			throw new DALException("Impossible d'inserer les donnees.", e);
+		}
+	}
+	
+	public void update(Card card) throws DALException {
+		try {
+			PreparedStatement ps = cnx.prepareStatement(UPDATE);
+			ps.setString(1, card.getName());
+			ps.setInt(2, card.getId());
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			throw new DALException("Impossible de mettre a jour les informations pour l'id "+ card.getId(), e);
+		}
+	}
+	
+	public void delete(int id) throws DALException {
+		try {
+			PreparedStatement ps = cnx.prepareStatement(DELETE);
+			ps.setInt(1, id);
+			int nbDeleteLine = ps.executeUpdate();
+			if(nbDeleteLine == 0) {
+				throw new DALException("Echec de suppression du composant d'id " + id, null);
+			}
+		} catch (SQLException e) {
+			throw new DALException("Impossible de supprimer le composant d'id "+ id, e);
+		}
+	}
+}
--- src/main/webapp/WEB-INF/jsp/JSPHome.jsp
+++ src/main/webapp/WEB-INF/jsp/JSPHome.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-   <%@ taglib prefix="c" uri="jakarta.tags.core" %>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
    
 <!DOCTYPE html>
 <html>
@@ -14,9 +14,11 @@
 
 
 	<c:forEach var="item" items="${restaurants}">
-		<article>
-		<h2>${item.name}</h2>
-		</article>
+		<a href="restaurant?id=${item.id }">
+			<article>
+			<h2>${item.name}</h2>
+			</article>
+		</a>
 	</c:forEach>
 
 </body>
