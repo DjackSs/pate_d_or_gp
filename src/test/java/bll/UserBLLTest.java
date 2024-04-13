e292e2c3904fa5980c7066e264b3a5768525dd9e
Nat <nathan.hamon.44@gmail.com>
2024-04-13 23:18:51 +0200

    first unit test are set up for userBLL

--- src/test/java/bll/UserBLLTest.java
+++ src/test/java/bll/UserBLLTest.java
@@ -1,34 +1,69 @@
 package bll;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class UserBLLTest {
-
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
-
+class UserBLLTest 
+{
+	private UserBLL userBll;
+	
+	//===============================
+	
 	@BeforeEach
-	void setUp() throws Exception {
+	void initUserBLL()
+	{
+		try 
+		{
+			this.userBll = new UserBLL();
+		} 
+		catch (BLLException e) 
+		{
+			e.printStackTrace();
+		}
 	}
 
 	@AfterEach
-	void tearDown() throws Exception {
+	void destroyUserBLL()
+	{
+		this.userBll = null;
+		
 	}
+	
+	//===============================
+	//selectByEmailAndPassword
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void selectByEmailAndPasswordWithEmailNullAndPasswordNullThrowBLLException()
+	{
+		assertThrows(BLLException.class, ()-> this.userBll.selectByEmailAndPassword(null, null), "selectByEmailAndPassword with email = null and password = null should throw BLLException");
+		
+	}
+	
+	//-----------------------------------
+	
+	@Test
+	void selectByEmailAndPasswordWithEmailEmptyAndPasswordEmptyThrowBLLException()
+	{
+		String emptyEmail = "                                 ";
+		String emptyPassword = "";
+		
+		assertThrows(BLLException.class, ()-> this.userBll.selectByEmailAndPassword(emptyEmail, emptyPassword), "selectByEmailAndPassword with empty email and empty password should throw BLLException");
+		
+	}
+	
+	//-----------------------------------
+	
+	@Test
+	void selectByEmailAndPasswordWithWrongEmailAndWrongPasswordTrowBLLException()
+	{
+		String wrongEmail = "email@wrong.com";
+		String wrongPassword = "wrong";
+		
+		assertThrows(BLLException.class, ()-> this.userBll.selectByEmailAndPassword(wrongEmail, wrongPassword), "selectByEmailAndPassword with wrong email and  wrong password should throw BLLException");
+		
 	}
 
 }
